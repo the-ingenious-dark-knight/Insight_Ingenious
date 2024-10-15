abbaf3ba0c3f6d6ea1633520002279baa047ae57
Eliot Zhu <oak_link0x@icloud.com>
2024-10-16 10:09:16 +1100

    update

--- autogen/extensions/ingenious/__init__.py
+++
@@ -1,5 +0,0 @@
-# pyautogen/extensions/__init__.py
-
-from .my_extension import my_function
-
-__all__ = ['my_function']
--- autogen/extensions/ingenious/my_extension.py
+++
@@ -1,4 +0,0 @@
-# pyautogen/extensions/my_extension.py
-
-def my_function():
-    return "Hello from my_extension!"
