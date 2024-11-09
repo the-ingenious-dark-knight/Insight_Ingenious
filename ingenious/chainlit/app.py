3306f50cdadebfb20950abfb5200458d4953cb99
Eliot Zhu <oak_link0x@icloud.com>
2024-11-09 15:05:02 +1100

    fix docker

--- ingenious/chainlit/app.py
+++ ingenious/chainlit/app.py
@@ -4,15 +4,15 @@ import ingenious.dependencies as deps
 import uuid
 
 @cl.on_message
-async def main(message: cl.Message):
+async def main(message: cl.Message, selected_flow: str = "sql_manipulation_agent", topic: str = ""):
     new_guid = uuid.uuid4()
     chat_request: ChatRequest = ChatRequest(
         thread_id= "demo_"+str(new_guid),
         user_id="Demo",
         user_prompt=message.content,
         user_name="Demo",
-        topic="",
-        conversation_flow="sql_manipulation_agent"
+        topic=topic,
+        conversation_flow=selected_flow
     )
 
     cs = deps.get_chat_service(
@@ -25,3 +25,4 @@ async def main(message: cl.Message):
     await cl.Message(
         content=ret.agent_response,
     ).send()
+
