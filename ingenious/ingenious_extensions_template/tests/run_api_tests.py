5ecc331cad007bdbd063df1f6621902ccde26406
Eliot Zhu <oak_link0x@icloud.com>
2025-01-11 18:22:50 +1100

    update doc

--- ingenious/ingenious_extensions_template/tests/run_api_tests.py
+++ ingenious/ingenious_extensions_template/tests/run_api_tests.py
@@ -48,13 +48,11 @@ async def main():
     if message_object:
         thread_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         user_prompt = jsonpickle.dumps(message_object, unpicklable=False)
-        event_type = 'is_wicket_ball'
+
         chat_request = ChatRequest(
             thread_id=thread_id,
             user_prompt=user_prompt,
-            conversation_flow="-----",
-            event_type=event_type
-        )
+            conversation_flow="-----"        )
 
         try:
             response = requests.post(
