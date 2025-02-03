36dcbdd8cbc443520ab7dd449d51d6ab4d6ede9b
John Rampono <john.rampono@insight.com>
2025-02-03 16:49:25 +0800

    Fix to add custom api routes

--- ingenious/ingenious_extensions_template/api/routes/custom.py
+++ ingenious/ingenious_extensions_template/api/routes/custom.py
@@ -1,19 +1,24 @@
 
 
 from urllib.error import HTTPError
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 from ingenious.models.api_routes import IApiRoutes
 
 
 class Api_Routes(IApiRoutes):
     def add_custom_routes(self) -> FastAPI:
-        @self.router.post(
+        router: APIRouter = APIRouter()
+
+        @router.post(
             "/chat2"
         )
         def chat2():
             # return success
             return "OK"
+        
+        self.app.include_router(
+            router, prefix="/api/v1", tags=["chat2"]
+        )
+        return self.app.router
 
-        return self.router
- 
--- ingenious/models/api_routes.py
+++ ingenious/models/api_routes.py
@@ -1,7 +1,7 @@
 
 from abc import ABC, abstractmethod
 import logging
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 from ingenious.models.config import Config
 from ingenious.models.http_error import HTTPError
 from ingenious.dependencies import get_security_service, get_chat_history_repository
@@ -10,10 +10,10 @@ from fastapi.security import HTTPBasicCredentials
 
 
 class IApiRoutes(ABC):
-    def __init__(self, config: Config, router: APIRouter):
+    def __init__(self, config: Config, app: FastAPI):
         self.config = config
         self.logger = logging.getLogger(__name__)
-        self.router = router
+        self.app = app
         self.security_service = get_security_service(HTTPBasicCredentials)
         self.chat_history_repository = get_chat_history_repository()
     
