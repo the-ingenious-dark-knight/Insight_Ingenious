4a8531eaa0522e666926b3db748d2570b5cdc911
manuelfdng-insight <Kokko.Ng@insight.com>
2025-05-22 11:22:06 +0800

    remove unused files

--- dev_scripts/guardrails/buildscripts/start.sh
+++
@@ -1,3 +0,0 @@
-docker stop guardrails-server || true
-docker rm guardrails-server || true
-docker run -p 8000:8000 -env AZURE_API_KEY AZURE_API_BASE AZURE_API_VERSION --name guardrails-server -it guardrails-server:dev
